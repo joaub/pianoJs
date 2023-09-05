98718f783507daf24ef458e5c0c06219e4608830
Joaquin Ugarte <ugartejoa12@gmail.com>
2023-09-05 12:13:12 -0300

    tocando el piano con numeros

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 const keys = document.querySelectorAll(".key");
+const notesArray = ["Do","Re","Mi","Fa","Sol","La","Si"];
+
+document.onkeydown = (key) => {
+    if(key.key <= 7 && key.key >= 1) {
+        let pressedKey = notesArray[key.key - 1];/*almaceno la llave*/
+        playNote(pressedKey); 
+        let div = Array.from(keys)[key.key -1];
+        div.classList.add("pressed");
+    }
+}
+
+document.onkeyup = () => {
+    keys.forEach((key)=>{key.classList.remove("pressed")});
+}
 
 keys.forEach(key => {
     key.addEventListener("click", function() {
